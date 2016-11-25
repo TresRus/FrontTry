3f36121da172734c78f7f01ea4b48ef4134d193a
Alexander Pisarev <tresrus92@gmail.com>
2016-11-25 19:00:57 +0300

    static: add base jQuery and AJAX scripts

---
+++ static/script/elements-jq.js
@@ -0,0 +1,11 @@
+$('#change-btn').click(function () {
+	$.ajax({
+		url: '/test.json',
+		method: 'GET',
+		success: function (data) {
+			$('#first-item').html(data.first);
+			$('#second-item').html(data.second);
+		}
+	});
+});
+
---
+++ static/script/elements.js
@@ -0,0 +1,12 @@
+function setListener (id, event, func) {
+	var element = document.getElementById(id);
+	element.addEventListener(event, func);
+}
+
+var changeLogic = function (event) {
+	alert("Everything is changing!!!");
+	alert(event);
+};
+
+setListener('change-btn', 'click', changeLogic);
+
