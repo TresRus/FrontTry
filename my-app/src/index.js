90443faadef82388afb53fcf326d04bca847206a
Alexander Pisarev <tresrus92@gmail.com>
2017-07-25 15:07:28 +0300

    components are here

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,31 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-const user = {
-    firstName: 'Harper',
-    lastName: 'Perez'
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-test">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}!</h1>
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
 };
 
-function tick() {
-    const element = (
-        <dev>
-            <h1>
-                Hello, {formatName(user)}!
-            </h1>
-            <h2>
-                It is {new Date().toLocaleTimeString()}.
-            </h2>
-        </dev>
-    );
+const element = (
+    <div>
+        <App />
+        <Comment
+            date={comment.date}
+            text={comment.text}
+            author={comment.author} />
+    </div>
+);
 
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
