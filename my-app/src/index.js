10924fcb201b567d7d83f79efd1382d627290e3a
Alexander Pisarev <tresrus92@gmail.com>
2017-07-25 14:37:37 +0300

    add time line

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+function tick() {
+    const element = (
+        <dev>
+            <h1>
+                Hello, {formatName(user)}!
+            </h1>
+            <h2>
+                It is {new Date().toLocaleTimeString()}.
+            </h2>
+        </dev>
+    );
+
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+}
+
+setInterval(tick, 1000);
