bf7526e864ab64aece5a8e8d3c3d620c69be2491
Alexander Pisarev <tresrus92@gmail.com>
2017-07-26 18:45:07 +0300

    create clock element

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -54,6 +54,39 @@ function App() {
     );
 }
 
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
 const comment = {
     date: new Date(),
     text: 'I hope you enjoy learning React!',
@@ -65,6 +98,7 @@ const comment = {
 
 const element = (
     <div>
+        <Clock />
         <App />
         <Comment
             date={comment.date}
