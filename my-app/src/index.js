4bb78aa30845e63ac11e3f35e727ccf03a239537
Alexander Pisarev <tresrus92@gmail.com>
2017-08-16 16:10:28 +0300

    Add table think

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -634,6 +634,83 @@ class SplitPane extends React.Component {
   }
 }
 
+class ProductCategoryRow extends React.Component {
+  render() {
+    return <tr><th colSpan="2">{this.props.category}</th></tr>
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    const name = (
+      <span className={'ProductRow ProductRow-' + (this.props.product.stocked ? 'ok' : 'out')}>
+        {this.props.product.name}
+      </span>
+    );
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
+  } 
+}
+
+class ProductTable extends React.Component {
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach(function(product) {
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+}
+
 class Page extends React.Component {
   constructor(props) {
     super(props);
@@ -672,6 +749,14 @@ class Page extends React.Component {
       mango: "Mango",
     }
 
+    const products = [
+      {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+      {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+      {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+      {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+      {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+      {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+    ]
 
     return (
       <div>
@@ -719,6 +804,7 @@ class Page extends React.Component {
           <FlavorForm flavours={flavours} />
           <Reservation />
           <Calculator />
+          <FilterableProductTable products={products} />
         </div>
       </div>
     );
